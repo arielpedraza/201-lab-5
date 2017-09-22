e7d65d9c4b7cd772f18bf0d7991d7130502bb8cc
Ariel R. Pedraza <pedrazaa2@gmail.com>
2017-09-22 13:35:48 -0700

    Finished Problem 2

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.
 
-For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
+For DONE item, be sure to change it to say DONE when you have successfully completed the task*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 function sum(a,b){ //eslint-disable-line
   var theSum = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
   return [theSum, message];
 }
 
-// TODO: Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+// DONE: Here is the test for sum(); uncomment it to run it
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,13 +30,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var theProduct = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + theProduct + '.';
+  return [theProduct, message];
 }
 
-// TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+// DONE: Here is the test for multiply(); uncomment it to run it
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
